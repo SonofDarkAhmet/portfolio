ea226f1671f2f469853e3023c768991751d8fa55
Raif Karaahmetoglu <raif.karaahmetoglu@avl.com>
2025-03-18 11:22:01 +0100

    Copy link is added.

--- src/components/Projects/ShareLink.jsx
+++ src/components/Projects/ShareLink.jsx
@@ -6,32 +6,79 @@ import {
   Fade,
   Typography,
   IconButton,
+  Snackbar,
+  Alert,
+  styled,
 } from "@mui/material";
 import ShareIcon from "@mui/icons-material/Share";
+import CancelPresentationIcon from "@mui/icons-material/CancelPresentation";
 
-const style = {
+const ModalShareLink = styled(Modal)({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+});
+
+const ModelBox = styled(Box)({
+  position: "relative",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundColor: "transparent",
+});
+
+const TextBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  width: "85%",
+  minHeight: "2rem",
+  backgroundColor: "black",
+  border: "0.2rem solid orange",
+  borderRadius: "1rem",
+  wordBreak: "break-word",
+  whiteSpace: "normal",
+  padding: "1% 2%",
+  cursor: "pointer",
+}));
+
+const ModalCancelIcon = styled(CancelPresentationIcon)({
   position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
+  right: "1.5rem",
+  top: "-1.2rem",
+  cursor: "pointer",
+});
 
-function ShareLink() {
+function ShareLink(props) {
   const [open, setOpen] = useState(false);
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const handleSnackbarClose = () => setSnackbarOpen(false);
+
+  const link =
+    props.link ||
+    "https://raifkaraahmetoglu.netlify.app/sdajdkawildjaopweoawjdiawjdklzsjfopapkwdajwdijwapodkawkdisdfhdhgsopfdosk";
+
+  function handleCopyLink() {
+    navigator.clipboard
+      .writeText(link)
+      .then(() => {
+        setSnackbarOpen(true);
+      })
+      .catch((error) => {
+        console.error("Failed to copy link: ", error);
+      });
+  }
 
   return (
-    <div>
+    <>
       <IconButton aria-label="share" onClick={handleOpen}>
         <ShareIcon />
       </IconButton>
-      <Modal
+      <ModalShareLink
         aria-labelledby="transition-modal-title"
         aria-describedby="transition-modal-description"
         open={open}
@@ -45,14 +92,34 @@ function ShareLink() {
         }}
       >
         <Fade in={open}>
-          <Box sx={style}>
-            <Typography id="transition-modal-title" variant="h6" component="h2">
-              Text in a modal
-            </Typography>
-          </Box>
+          <ModelBox>
+            <ModalCancelIcon onClick={handleClose} />
+            <TextBox onClick={handleCopyLink}>
+              <Typography
+                id="transition-modal-title"
+                variant="body1"
+                color="secondary"
+              >
+                {link}
+              </Typography>
+            </TextBox>
+          </ModelBox>
         </Fade>
-      </Modal>
-    </div>
+      </ModalShareLink>
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={3000}
+        onClose={handleSnackbarClose}
+      >
+        <Alert
+          onClose={handleSnackbarClose}
+          severity="success"
+          sx={{ width: "100%" }}
+        >
+          Link copied to clipboard!
+        </Alert>
+      </Snackbar>
+    </>
   );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -5,10 +5,10 @@ import App from "./App";
 
 let theme = createTheme({
   palette: {
-    background: { default: "#f0f2f5" },
+    background: { default: "#000" },
     secondary: {
-      main: "#ffffff", // Change this to your desired secondary color
-      light: "#ff79b0", // Optional: lighter shade of secondary color
+      main: "#f0f2f5", // Change this to your desired secondary color
+      light: "#ffffff", // Optional: lighter shade of secondary color
       dark: "#D4D4D4", // Optional: darker shade of secondary color
       contrastText: "#ffffff", // Optional: text color to use on secondary color
     },
